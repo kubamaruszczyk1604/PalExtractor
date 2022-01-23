9bafe1434060a44305c63c9c48927e0b3b2f9912
Kuba Maruszczyk <kmaruszczyk1604@gmail.com>
2022-01-23 15:04:50 +0000

    color conversions

--- ImageConverter/Form1.cs
+++ ImageConverter/Form1.cs
@@ -46,123 +46,37 @@ namespace ImageConverter
             {
                 for (int j = 0; j < scrBitmap.Height; j++)
                 {
-                    //scrBitmap.SetPixel(i, j, Color.Azure);
-                    //Color c = scrBitmap.GetPixel(i, j);
-                    //float h = c.GetHue();
-                    //float s = c.GetSaturation();
-                    //float v = c.GetBrightness();
-                    //scrBitmap.SetPixel(i, j, HSV2RGB(h, s, v));
-                    
+                    Color c = scrBitmap.GetPixel(i, j);
+                    Color2Hsv(c, out double h, out double s, out double v);
+                    scrBitmap.SetPixel(i, j, Hsv2Color((float)h, (float)s, (float)v));
+
                 }
 
             }
         }
-       static  Color HSV2RGB(float h, float s, float v)
+
+
+
+
+       static  Color Hsv2Color(float h, float s, float v)
         {
-            HsvToRgb(h, s, v, out int r, out int g, out int b);
+            ColorConverter.Hsv2Rgb(h, s, v, out int r, out int g, out int b);
             return Color.FromArgb(r, g, b);
         }
 
-        static void HsvToRgb(double h, double S, double V, out int r, out int g, out int b)
+        public static void Color2Hsv(Color color, out double hue, out double saturation, out double value)
         {
-            double H = h;
-            while (H < 0) { H += 360; };
-            while (H >= 360) { H -= 360; };
-            double R, G, B;
-            if (V <= 0)
-            { R = G = B = 0; }
-            else if (S <= 0)
-            {
-                R = G = B = V;
-            }
-            else
-            {
-                double hf = H / 60.0;
-                int i = (int)Math.Floor(hf);
-                double f = hf - i;
-                double pv = V * (1 - S);
-                double qv = V * (1 - S * f);
-                double tv = V * (1 - S * (1 - f));
-                switch (i)
-                {
+            int max = Math.Max(color.R, Math.Max(color.G, color.B));
+            int min = Math.Min(color.R, Math.Min(color.G, color.B));
 
-                    // Red is the dominant color
-
-                    case 0:
-                        R = V;
-                        G = tv;
-                        B = pv;
-                        break;
-
-                    // Green is the dominant color
-
-                    case 1:
-                        R = qv;
-                        G = V;
-                        B = pv;
-                        break;
-                    case 2:
-                        R = pv;
-                        G = V;
-                        B = tv;
-                        break;
-
-                    // Blue is the dominant color
-
-                    case 3:
-                        R = pv;
-                        G = qv;
-                        B = V;
-                        break;
-                    case 4:
-                        R = tv;
-                        G = pv;
-                        B = V;
-                        break;
-
-                    // Red is the dominant color
-
-                    case 5:
-                        R = V;
-                        G = pv;
-                        B = qv;
-                        break;
-
-                    // Just in case we overshoot on our math by a little, we put these here. Since its a switch it won't slow us down at all to put these here.
-
-                    case 6:
-                        R = V;
-                        G = tv;
-                        B = pv;
-                        break;
-                    case -1:
-                        R = V;
-                        G = pv;
-                        B = qv;
-                        break;
-
-                    // The color is not defined, we should throw an error.
-
-                    default:
-                        //LFATAL("i Value error in Pixel conversion, Value is %d", i);
-                        R = G = B = V; // Just pretend its black/white
-                        break;
-                }
-            }
-            r = Clamp((int)(R * 255.0));
-            g = Clamp((int)(G * 255.0));
-            b = Clamp((int)(B * 255.0));
+            hue = color.GetHue();
+            saturation = (max == 0) ? 0 : 1d - (1d * min / max);
+            value = max / 255d;
         }
 
-        /// <summary>
-        /// Clamp a value to 0-255
-        /// </summary>
-        static int Clamp(int i)
-        {
-            if (i < 0) return 0;
-            if (i > 255) return 255;
-            return i;
-        }
+
+
+
 
     }
 }
