c433745e1e8787aea708b9907b5569e685881c42
Kuba Maruszczyk <kmaruszczyk1604@gmail.com>
2022-01-23 18:32:52 +0000

    snc

--- ImageConverter/KMeansClustering.cs
+++ ImageConverter/KMeansClustering.cs
@@ -82,7 +82,7 @@ namespace KMeans
                     distChanged += m_Clusters[iCluster].RecalculateCentroid();
                 }
                 Console.WriteLine("Convergence = " + distChanged);
-                if (distChanged < 0.001)
+                if (distChanged < 0.01)
                     break;
             }
             return m_Clusters;
